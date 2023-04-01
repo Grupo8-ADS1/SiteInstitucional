88c1610f19b8585727da929ddd835a18fde316ea
JohnVSCordeiro <joao.cordeiro@sptech.school>
2023-04-01 12:40:06 -0300

    @JohnVSCordeiro subindo banco de dados para aprovação da professora

--- Mercurio/BD/BDMercurio.sql
+++ Mercurio/BD/BDMercurio.sql
@@ -1,9 +1,11 @@
+create database mercurio;
 use mercurio;
 
 create table sensor (
 idSensor int primary key auto_increment,
 -- fkHistorico int not null,
-fkEspaco varchar(20) not null, constraint chkNivel check (fkEspaco in ('Entrada','Saida'))
+fkEspaco varchar(20) not null, -- constraint fkEsp foreign key (fkEspaco) references Espaco (idEspaco)   
+constraint chktipo check (fkEspaco in ('Entrada','Saida'))
 );
 
 
@@ -20,7 +22,7 @@ nivelUsuario varchar(20) not null, constraint chkNivel check (nivelUsuario in ('
 create table espaco (
 idEspaco int primary key auto_increment,
 setor varchar(50) not null,
-fkSensor int not null
+fkSensor int not null, constraint fkSenE foreign key (fkSensor) references Sensor (idSensor) 
 );
 
 
@@ -33,14 +35,18 @@ senhaEmpresa varchar(50) not null
 );
 
 create table captacao(
-fkSensor int not null,
-dtCaptacao datetime default current_timestamp,
-fkEmpresa int 
+fkSensor int not null,constraint fkSenC foreign key (fkSensor) references sensor (idSensor),
+dtCaptacao datetime default current_timestamp, 
+fkEmpresa int, constraint fkEmp foreign key (fkEmpresa) references empresa (idEmpresa)
 );
 
 
+
+/*
 
 insert into usuario values 
 (null,'João Vitor Cordeiro','123456789','joao.cordeiro@sptech.school','GerenteRegional'),
 (null,'Felipe Andrade','123456789','felipe.andrade@sptech.school','GerenteLocal'),
 (null,'Evelyn Farias','123456789','evelyn.farias@sptech.school','GerenteRegional');
+
+*/ 
