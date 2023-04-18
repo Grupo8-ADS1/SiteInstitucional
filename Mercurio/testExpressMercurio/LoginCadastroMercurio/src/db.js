18bba9d51d4be688e95335cdb15d78d6657fab31
LucioGarciaRios <lucio.rios@sptech.school>
2023-04-18 15:08:54 -0300

    Atualização tela de cadastro, login e quem somos

--- Mercurio/testExpressMercurio/LoginCadastroMercurio/src/db.js
+++ Mercurio/testExpressMercurio/LoginCadastroMercurio/src/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2'); // função de conectar com o banco
 
 const connection = mysql.createConnection({ // criando a conexão com o banco
     host: 'localhost',  // server local
-    user: 'evelyn', // usuario do banco local
-    password: 'sptech', // senha do banco local
+    user: 'root', // usuario do banco local
+    password: 'Lucio0512', // senha do banco local
     database: 'mercurio' // database do site
 });
 
